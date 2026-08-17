8c77b4d2b6c0555afc6003ef50179e7019448fcd
Abhinav Pathak <abhinavpathak789@gmail.com>
2026-08-18 04:42:52 +0530

    feat: add task activity model

---
+++ multitenant-saas/src/main/java/com/chacha/multitenantsaas/entity/TaskActivityType.java
@@ -0,0 +1,12 @@
+package com.chacha.multitenantsaas.entity;
+
+public enum TaskActivityType {
+    TASK_CREATED,
+    TASK_UPDATED,
+    STATUS_CHANGED,
+    ASSIGNEE_CHANGED,
+    TASK_CANCELLED,
+    COMMENT_ADDED,
+    COMMENT_EDITED,
+    COMMENT_DELETED
+}
