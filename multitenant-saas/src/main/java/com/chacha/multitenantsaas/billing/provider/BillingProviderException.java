7d15dad6f3363eefa8488efb1b2728921650bb81
Abhinav Pathak <abhinavpathak789@gmail.com>
2026-08-22 21:58:48 +0530

    feat: add Stripe test-mode billing adapter

---
+++ multitenant-saas/src/main/java/com/chacha/multitenantsaas/billing/provider/BillingProviderException.java
@@ -0,0 +1,8 @@
+package com.chacha.multitenantsaas.billing.provider;
+
+public class BillingProviderException extends RuntimeException {
+
+    public BillingProviderException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
