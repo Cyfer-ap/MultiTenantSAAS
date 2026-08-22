e99cec9ba198f8a8d97f3cadb86a6955c49f5e02
Abhinav Pathak <abhinavpathak789@gmail.com>
2026-08-22 19:02:38 +0530

    test: advance PostgreSQL schema expectation to V28

--- multitenant-saas/src/test/java/com/chacha/multitenantsaas/integration/PostgreSqlSchemaIntegrationTest.java
+++ multitenant-saas/src/test/java/com/chacha/multitenantsaas/integration/PostgreSqlSchemaIntegrationTest.java
@@ -69,7 +69,7 @@ class PostgreSqlSchemaIntegrationTest {
     @Autowired private UserOrganizationAssignmentRepository userOrganizationAssignmentRepository;
 
     @Test
-    void postgresSchemaReachesV27AndMatchesJpaMappings() {
+    void postgresSchemaReachesV28AndMatchesJpaMappings() {
         String version =
                 jdbcTemplate.queryForObject(
                         """
@@ -81,7 +81,7 @@ class PostgreSqlSchemaIntegrationTest {
                 """,
                         String.class);
 
-        assertThat(version).isEqualTo("27");
+        assertThat(version).isEqualTo("28");
 
         Integer permissionCount =
                 jdbcTemplate.queryForObject(
@@ -104,6 +104,8 @@ class PostgreSqlSchemaIntegrationTest {
         assertTableExists("notifications");
         assertTableExists("notification_deliveries");
         assertTableExists("notification_preferences");
+        assertTableExists("billing_customers");
+        assertTableExists("billing_events");
         assertColumnExists("task_attachments", "storage_deleted_at");
         assertColumnExists("task_comments", "parent_comment_id");
         assertColumnExists("task_comments", "reply_count");
