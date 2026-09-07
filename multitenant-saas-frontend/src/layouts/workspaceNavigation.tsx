840094f4f3cc8db484ba16e20d1a9209ed25c0ee
Abhinav Pathak <abhinavpathak789@gmail.com>
2026-09-07 21:01:44 +0530

    feat: expose integrations in workspace navigation

--- multitenant-saas-frontend/src/layouts/workspaceNavigation.tsx
+++ multitenant-saas-frontend/src/layouts/workspaceNavigation.tsx
@@ -6,6 +6,7 @@ import GroupsRoundedIcon from '@mui/icons-material/GroupsRounded'
 import HistoryRoundedIcon from '@mui/icons-material/HistoryRounded'
 import PaymentsRoundedIcon from '@mui/icons-material/PaymentsRounded'
 import PersonAddAltRoundedIcon from '@mui/icons-material/PersonAddAltRounded'
+import WebhookRoundedIcon from '@mui/icons-material/WebhookRounded'
 import type { ReactNode } from 'react'
 
 import {
@@ -62,6 +63,12 @@ export const workspaceNavigationItems: readonly WorkspaceNavigationItem[] = [
         path: '/authorization',
         icon: <AdminPanelSettingsRoundedIcon />,
         requiredTenantPermissions: [authorizationPermissionCodes.AUTHORIZATION_MANAGE],
+    },
+    {
+        label: 'Integrations',
+        path: '/integrations',
+        icon: <WebhookRoundedIcon />,
+        requiredTenantPermissions: [authorizationPermissionCodes.TENANT_UPDATE],
     },
     {
         label: 'Subscription',
