900812bfce9a76461e0b1046322312ecbcae235c
Abhinav Pathak <abhinavpathak789@gmail.com>
2026-09-23 18:07:21 +0530

    feat: allow explicit approval review grants

--- multitenant-saas/src/main/java/com/chacha/multitenantsaas/externalaccess/ExternalAccessGrantService.java
+++ multitenant-saas/src/main/java/com/chacha/multitenantsaas/externalaccess/ExternalAccessGrantService.java
@@ -144,7 +144,7 @@ public class ExternalAccessGrantService {
                 && !capabilities.contains(ExternalAccessCapability.TASK_READ)) {
             throw new IllegalArgumentException("TASK_COMMENT_CREATE requires TASK_READ");
         }
-        if (capabilities.size() > 3) {
+        if (capabilities.size() > 4) {
             throw new IllegalArgumentException("External grant capability set is too large");
         }
     }
